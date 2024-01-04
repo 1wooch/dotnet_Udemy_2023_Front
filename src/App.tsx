a39e5f57af132eec51c2d660146ac682a68cf2e5
wwc <onechoo1106@gmail.com>
2024-01-04 21:35:02 +1000

    finish 03-25

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   );
 
   useEffect(()=>{
-    fetch('http://localhost:5000/api/products')
+    fetch('http://localhost:5198/api/products')
     .then(response => response.json())
     .then(data=>setProducts(data))
 
