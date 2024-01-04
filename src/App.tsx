56d166468c2c641c0c37f2b87d5d5abacdd71340
wwc <onechoo1106@gmail.com>
2024-01-04 20:41:46 +1000

    finish 03-22

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+const products = [
+  {
+    name: 'Product 1',
+    price:100.00
+  },
+  {
+    name: 'Product 2',
+    price:200.00
+  },
+  {
+    name: 'Product 3',
+    price:300.00
+  }
+]
 
 function App() {
 
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - ${product.price}
+          </li>
+        ))}        
+      </ul>
     </div>
   )
 }
