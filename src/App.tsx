36a9d7df35198d11676ad0fe8b94c7d4048632a7
wwc <onechoo1106@gmail.com>
2024-01-09 21:57:59 +1000

    finish 03-27

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { useEffect, useState } from "react";
+import { Product } from "./Product";
 
 function App() {
-  const [products, setProducts] = useState(
+  const [products, setProducts] = useState<Product[]>(
     [
-      {
-        name: 'Product 1',
-        price:100.00
-      },
-      {
-        name: 'Product 2',
-        price:200.00
-      },
-      {
-        name: 'Product 3',
-        price:300.00
-      }
+    
     ]
   );
 
@@ -27,15 +17,24 @@ function App() {
   
 
   function addProduct() {
-    setProducts(prevState=>[...prevState,{name:'Product'+(prevState.length+1),price:(prevState.length+1)*100.00}]);
+    setProducts(prevState=>[...prevState,
+      {
+        id:prevState.length+101,
+        name:'Product'+(prevState.length+1),
+        price:(prevState.length+1)*100.00,
+        brand:"random brand",
+        description:"random description",
+        pictureUrl:"http://picsum.photos/200",
+       
+      }]);
   }
 
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>
+        {products.map(product => (
+          <li key={product.id}>
             {product.name} - ${product.price}
           </li>
         ))}        
