a3b18ec92c9c00ded87cdc4583e838d3869302bd
wwc <onechoo1106@gmail.com>
2024-01-12 18:27:36 +1000

    finish 03-34

--- src/feature/catalog/ProductCard.tsx
+++ src/feature/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/Product";
 
 interface Props{
@@ -8,11 +8,25 @@ interface Props{
 
 export default function ProductCard({product}:Props){
     return(
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl}/>
-            </ListItemAvatar>
-            <ListItemText>{product.name} - ${product.price}</ListItemText>
-        </ListItem>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={product.pictureUrl}
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )   
 }
