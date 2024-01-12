ad77efb14861cecb7429728588109a6b94ae1662
wwc <onechoo1106@gmail.com>
2024-01-12 17:51:12 +1000

    finish 03-31

--- src/feature/catalog/Catalog.tsx
+++ src/feature/catalog/Catalog.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {Button } from "@mui/material";
 import React from "react";
 import { Product } from "../../app/models/Product";
+import ProductList from "./ProductList";
 
 interface Props{
     products:Product[],
@@ -12,17 +13,7 @@ export default function Catalog({products,addProduct}:Props){
 
     return(
         <React.Fragment>
-            <List>
-                {products.map(product => (
-                <ListItem key={product.id}>
-                    <ListItemAvatar>
-                        <Avatar src={product.pictureUrl}/>
-                    </ListItemAvatar>
-                    <ListItemText>{product.name} - ${product.price}</ListItemText>
-                    
-                </ListItem>
-                ))}        
-            </List>
+            <ProductList products={products}/>
             <Button variant='contained' onClick={addProduct}>Add Product</Button>
         </React.Fragment>
     )
