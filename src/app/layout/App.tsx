483de88099a5b13022e1a9f0b865ee9481d548ed
wwc <onechoo1106@gmail.com>
2024-01-29 22:20:30 +1000

    finish 40

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Catalog from "../../feature/catalog/Catalog";
 import Header from "./Heder";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 
 function App() {
@@ -27,7 +28,7 @@ function App() {
         <CssBaseline/> 
             <Header darkMode={darkMode} handleThemeChange={onChange} />
             <Container>
-            <Catalog/>
+            <Outlet/>
         </Container>
       </ThemeProvider>
       
---
+++ src/app/router/Rotes.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../layout/App";
+import HomePage from "../../feature/home/HomePage";
+import ContactPage from "../../feature/contact/ContactPage";
+import AboutPage from "../../feature/about/AboutPage";
+import Catalog from "../../feature/catalog/Catalog";
+import ProductDetails from "../../feature/catalog/ProductDetails";
+
+export const router =createBrowserRouter([
+    {
+        path: "/",
+        element:<App/>,
+        children:[
+            {
+                path:"",
+                element:<HomePage/>
+            },
+            {
+                path:"contact",
+                element:<ContactPage/>
+            },
+            {
+                path:"about",
+                element:<AboutPage/>
+            },
+            {
+                path:"catalog",
+                element:<Catalog/>
+            },
+            {
+                path:"catalog/:id",
+                element:<ProductDetails/>
+            },
+           
+        ]
+    }
+]);
