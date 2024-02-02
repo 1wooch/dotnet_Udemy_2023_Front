11f318e6bbd32c389289c1f93f12b28c0561c56d
wwc <onechoo1106@gmail.com>
2024-02-03 00:01:26 +1000

    clean up the code

--- src/index.tsx
+++ src/index.tsx
@@ -14,5 +14,5 @@ import { router } from './app/router/Rotes.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
