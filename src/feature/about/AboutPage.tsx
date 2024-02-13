6257a666f66e39ae57eb4676e773ce5921e04b72
wwc <onechoo1106@gmail.com>
2024-02-13 20:16:51 +1000

    finish 50 for error handling for FE

--- src/feature/about/AboutPage.tsx
+++ src/feature/about/AboutPage.tsx
@@ -1,8 +1,18 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage(){
     return(
-        <Typography variant="h2">About Page</Typography>
+        <Container>
+            <Typography gutterBottom variant="h2">Errors for testing purpose</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant ='contained' onClick={()=> agent.TestErrors.get400}>Test 400 Error</Button>
+                <Button variant ='contained' onClick={()=> agent.TestErrors.get401}>Test 401 Error</Button>
+                <Button variant ='contained' onClick={()=> agent.TestErrors.get404}>Test 404 Error</Button>
+                <Button variant ='contained' onClick={()=> agent.TestErrors.get500}>Test 500 Error</Button>
+                <Button variant ='contained' onClick={()=> agent.TestErrors.getValidationError}>Test getValidationError </Button>
+            </ButtonGroup>
+        </Container>
     )
     
 }
