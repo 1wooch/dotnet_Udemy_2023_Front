21e1d929670d8a6fa0b871843df467ae88a1a5eb
wwc <onechoo1106@gmail.com>
2024-03-05 21:05:14 +1000

    finish 74 add basket call from cookie and store it at highst component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Heder";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 
 function App() {
+  const {setBasket}=useStoreContext();
+  const [loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+    const buyerId=getCookie('buyerId');
+    if(buyerId){
+      agent.Basket.get().then(basket=>{
+        setBasket(basket);
+        setLoading(false);
+      }).catch(error=>console.log(error))
+      .finally(()=>{
+        setLoading(false);
+      })
+    }
+  },[setBasket]);
+
+
+
   const [darkMode,setDarkMode] = useState(false);
   const palletType = darkMode ? "dark" : "light";
 
@@ -22,7 +44,7 @@ function App() {
   function onChange(){
     setDarkMode(!darkMode);
   }
-
+  if(loading) return <LoadingComponent message="Initialising App ..."/>;
   return (
     <div>
       <ThemeProvider theme={theme}>
