95898f518db1a5607e31fc9efd47a9f87d6e09b2
wwc <onechoo1106@gmail.com>
2024-03-13 20:51:37 +1000

    add redux and apply it on contact page #85

--- src/feature/contact/ContactPage.tsx
+++ src/feature/contact/ContactPage.tsx
@@ -1,8 +1,20 @@
 import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { CounterState } from "./counterReducer";
+import React from "react";
 
 export default function ContactPage(){
+    const {data,title} = useSelector((state:CounterState) => state);
     return(
-        <Typography variant="h2">Contact Page</Typography>
+        <React.Fragment>
+            <Typography variant="h2">
+                {title}
+            </Typography>  
+            <Typography variant="h2">
+                This Data is: {data}
+            </Typography>   
+        </React.Fragment>
+
     )
     
 }
