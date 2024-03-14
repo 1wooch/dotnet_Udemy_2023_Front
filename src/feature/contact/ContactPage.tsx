3695423b9a4bca59c495a486b7893401d9fdb483
wwc <onechoo1106@gmail.com>
2024-03-14 22:37:43 +1000

    add reduxfunction #86

--- src/feature/contact/ContactPage.tsx
+++ src/feature/contact/ContactPage.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
-import { CounterState } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { CounterState, DECREMENT_COUNTER, INCREMENT_COUNTER } from "./counterReducer";
 import React from "react";
 
 export default function ContactPage(){
+    const dispatch = useDispatch();
+
     const {data,title} = useSelector((state:CounterState) => state);
     return(
         <React.Fragment>
@@ -13,6 +15,14 @@ export default function ContactPage(){
             <Typography variant="h2">
                 This Data is: {data}
             </Typography>   
+            <ButtonGroup>
+                <Button onClick={()=>{dispatch({type:DECREMENT_COUNTER})}} variant="contained" color="error">
+                    Decrement
+                </Button>
+                <Button onClick={()=>{dispatch({type:INCREMENT_COUNTER})}} variant="contained" color="error">
+                    Increment
+                </Button>
+            </ButtonGroup>
         </React.Fragment>
 
     )
