735a78685a8fc3d5d66fba3ddc90af98e8c7ca5a
wwc <onechoo1106@gmail.com>
2024-03-18 19:36:12 +1000

    use redux toolkit and change into it #88

--- src/feature/contact/ContactPage.tsx
+++ src/feature/contact/ContactPage.tsx
@@ -1,13 +1,13 @@
 import { Button, ButtonGroup, Typography } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { CounterState, decrement, increment } from "./counterReducer";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { decrement, increment } from "./counterSlice";
 
 export default function ContactPage(){
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
-    const data = useSelector((state:CounterState) => state.data);
-    const title = useSelector((state:CounterState) => state.title);
+    const data = useAppSelector(state=>state.counter.data)
+    const title = useAppSelector(state=>state.counter.title)
 
     return(
         <React.Fragment>
@@ -18,10 +18,10 @@ export default function ContactPage(){
                 This Data is: {data}
             </Typography>   
             <ButtonGroup>
-                <Button onClick={()=>{dispatch(decrement())}} variant="contained" color="error">
+                <Button onClick={()=>{dispatch(decrement(1))}} variant="contained" color="error">
                     Decrement
                 </Button>
-                <Button onClick={()=>{dispatch(increment())}} variant="contained" color="primary">
+                <Button onClick={()=>{dispatch(increment(1))}} variant="contained" color="primary">
                     Increment
                 </Button>
                 <Button onClick={()=>{dispatch(increment(5))}} variant="contained" color="secondary">
