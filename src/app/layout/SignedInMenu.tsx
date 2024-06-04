b8a257869dbbec91fe46b410cceceadf138b9b0f
wwc <onechoo1106@gmail.com>
2024-06-04 20:36:07 +1000

    finish basket #144

--- src/app/layout/SignedInMenu.tsx
+++ src/app/layout/SignedInMenu.tsx
@@ -1,42 +1,45 @@
 import { Button, Fade, Menu, MenuItem } from "@mui/material";
-import React from "react";
+import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store/configureStore";
 import { signOut } from "../../feature/account/accountSlice";
+import { clearBasket } from "../../feature/basket/basketSlice";
 
-export default function SignedInMenu(){
-
+export default function SignedInMenu() {
     const dispatch = useAppDispatch();
-    const {user} = useAppSelector(state => state.account)
-
-
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const { user } = useAppSelector(state => state.account);
+    const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
+  
+    const handleClick = (event: any) => {
+      setAnchorEl(event.currentTarget);
     };
+  
     const handleClose = () => {
-        setAnchorEl(null);
+      setAnchorEl(null);
     };
-
+  
     return (
-        <>
+      <>
         <Button
-            color='inherit'
-            onClick={handleClick}
-            sx={{typography: 'h6'}}
+          color='inherit'
+          onClick={handleClick}
+          sx={{ typography: 'h6' }}
         >
-            {user?.email}
+          {user?.email}
         </Button>
         <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            TransitionComponent={Fade}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
         >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My Orders</MenuItem>
-            <MenuItem onClick={()=>{dispatch(signOut())}}>Logout</MenuItem>
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem divider onClick={handleClose}>My orders</MenuItem>
+          <MenuItem onClick={() => {
+            dispatch(signOut());
+            dispatch(clearBasket());
+          }}>Logout</MenuItem>
         </Menu>
-        </>
+      </>
     );
-}
+  }
--- src/feature/basket/basketSlice.ts
+++ src/feature/basket/basketSlice.ts
@@ -62,6 +62,9 @@ export const basketSlice = createSlice({
     reducers: {
         setBasket: (state, action) => {
             state.basket = action.payload
+        },
+        clearBasket: (state) => {
+            state.basket = null;
         }
     },
     extraReducers: builder => {
@@ -104,4 +107,4 @@ export const basketSlice = createSlice({
     }
 })
 
-export const {setBasket} = basketSlice.actions;
+export const {setBasket, clearBasket} = basketSlice.actions;
